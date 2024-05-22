406f236551fa0560557176596f8c080048c72e67
makwerik <serezha0790@mail.ru>
2024-05-22 15:32:22 +0300

    lesson

---
+++ 83.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def deleteDuplicates(self, head):
+        current = head
+        while current is not None and current.next is not None:
+            if current.next.val == current.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
+
+
+"""Но мне по-душе вот такое решение: =)"""
+
+"""
+    def deleteDuplicates(head):
+
+    result = []
+
+    for h in head:
+        if h not in result:
+            result.append(h)
+    return result
+"""
